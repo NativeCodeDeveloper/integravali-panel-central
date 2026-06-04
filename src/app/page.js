a762f6b67ebf10235816b90c6c8e2855674410be
NativeCode <tm.nicolasmachuca@gmail.com>
2026-06-04 16:25:26 -0400

    asdasdad

--- src/app/page.js
+++ src/app/page.js
@@ -35,11 +35,6 @@ export default function Home() {
           nombre: "Johane Fuentealba Díaz",
           especialidad: "Terapeuta Ocupacional",
           href: "https://integravali06.angendaclinica.xyz/agendaEspecificaProfersional/25",
-      }, {
-          id: "07",
-          nombre: "Catalina Javiera Leiva Videla",
-          especialidad: "Fonoaudióloga",
-          href: "https://integravali07.angendaclinica.boats/agendaEspecificaProfersional/23",
       },
 
   ];
